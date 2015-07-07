88331c6961d98b30a81c20723dfaadd0862f6c32
InFinitySqrd <jFinlayson94@gmail.com>
2015-07-07 13:08:09 +1000

    Added basics for Debug Menu, updated Task List

---
+++ Assets/Scripts/DebugMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugMenu : MonoBehaviour {
+	// Declare variables
+	// Boolean to check whether debug is enabled or not
+	bool debug = false;
+
+	// Use this for initialization
+	void Awake () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void SetUpDebugMenu() {
+
+	}
+
+	public void SwitchDebugState() {
+		debug = !debug;
+	}
+}
